a3bd2697b6dda43b189055f2180dfd68a1946a68
Roshan Sadha Sanker <70261891+RoshanBro77@users.noreply.github.com>
2022-10-15 22:33:12 +0545

    Space Complexity

--- HappyNumber.java
+++ HappyNumber.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class HappyNumber{
+    // O(1) space Complexity
     public static void main(String[] args) {
         System.out.println();  
     }
