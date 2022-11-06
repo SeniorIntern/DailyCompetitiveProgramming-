516c5cb8e97cd764353ffde9c7aa6a502ee2a9bd
Nikhil Thapa <tnikhil.official@gmail.com>
2022-11-06 23:23:48 +0545

    problem08

---
+++ Running_Sum_1d_Array.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+/*
+1480_Link: https://leetcode.com/problems/running-sum-of-1d-array/
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+*/
+public class Running_Sum_1d_Array {
+    public int[] runningSum(int[] nums) {
+        int sum =0;
+        for(int i =0; i< nums.length; i++){
+            nums[i] = nums[i]+sum;
+            sum=nums[i];
+        }
+        return nums;
+    }
+
+    //    Driver Code
+    public static void main(String[] args) {
+        Running_Sum_1d_Array oj = new Running_Sum_1d_Array();
+        System.out.println("output: "+ Arrays.toString(oj.runningSum(new int[]{1, 2, 3, 4})));
+    }
+}
