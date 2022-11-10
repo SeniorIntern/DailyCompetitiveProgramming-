02a130c4408f66b95c10fbca26895ce6aa830a44
Nikhil Thapa <tnikhil.official@gmail.com>
2022-11-10 21:49:22 +0545

    problem 11

--- Shuffle_the_Array.java
+++ Shuffle_the_Array.java
@@ -1,8 +1,8 @@
 /*
-1470_Link: https://leetcode.com/problems/shuffle-the-array/
-Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
-Return the array in the form [x1,y1,x2,y2,...,xn,yn].
-*/
+ * 1470_Link: https://leetcode.com/problems/shuffle-the-array/ Given the array nums consisting of 2n
+ * elements in the form [x1,x2,...,xn,y1,y2,...,yn]. Return the array in the form
+ * [x1,y1,x2,y2,...,xn,yn].
+ */
 
 import java.util.Arrays;
 
@@ -19,7 +19,8 @@ public class Shuffle_the_Array {
 
     // Driver Code
     public static void main(String[] args) {
-        Solution oj = new Solution();
-        System.out.println("output: " + Arrays.toString(oj.shuffle(new int[] { 1, 2, 3, 4, 4, 3, 2, 1 }, 4)));
+        Shuffle_the_Array oj = new Shuffle_the_Array();
+        System.out.println(
+                "output: " + Arrays.toString(oj.shuffle(new int[] {1, 2, 3, 4, 4, 3, 2, 1}, 4)));
     }
 }
