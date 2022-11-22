ec91fb2b91902ad4e4d01971fbfeb57f39de3509
Nikhil Thapa <tnikhil.official@gmail.com>
2022-11-22 21:59:14 +0545

    problem 24

---
+++ FindNumbers_withEvenNumber_ofDigits.java
@@ -0,0 +1,28 @@
+// 1295_Link: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+/*
+Given an array nums of integers, return how many of them contain an even number of digits.
+*/
+public class  FindNumbers_withEvenNumber_ofDigits {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        int p = 0;
+        for (int i : nums) {
+            while (i != 0) {
+                i /= 10;
+                p++;
+            }
+            System.out.println("num count: " + p);
+            if (p % 2 == 0) {
+                count += 1;
+            }
+            p = 0;
+        }
+        return count;
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        FindNumbers_withEvenNumber_ofDigits oj = new FindNumbers_withEvenNumber_ofDigits();
+        System.out.println("output:" + oj.findNumbers(new int[] {12, 345, 2, 6, 7896}));
+    }
+}
