01d224ea649643060b00b1030e092d1d0b82728a
Nikhil Thapa <tnikhil.official@gmail.com>
2022-12-10 22:42:08 +0545

    problem

---
+++ RemoveDuplicates_from_SortedArray.java
@@ -0,0 +1,11 @@
+public class RemoveDuplicates_from_SortedArray {
+    public int removeDuplicates(int[] nums) {
+        // O(n) O(1)
+        int index = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[index] != nums[i])
+                nums[++index] = nums[i];
+        }
+        return index + 1;
+    }
+}
